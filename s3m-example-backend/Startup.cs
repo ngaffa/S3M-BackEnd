93630b9ee64dec74ca639be9d244b03967ace7f5
ng <naoufal.gaffa@gmail.com>
2020-12-29 18:42:06 +0100

    finalizing s3m example

--- s3m-example-backend/Startup.cs
+++ s3m-example-backend/Startup.cs
@@ -33,7 +33,7 @@ namespace WebApplication2
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication2", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "S3M", Version = "v1" });
             });
         }
 
@@ -44,7 +44,7 @@ namespace WebApplication2
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication2 v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "S3M v1"));
             }
             app.UseCors("AllowOrigin");
             app.UseHttpsRedirection();
